3b1f0c0330f42529a8dc4eeb638d69cdab043f13
Denzel027 <denzeltdomino@iskolarngbayan.pup.edu.ph>
2026-05-28 23:03:59 +0800

    Finished About us, and navbar.

---
+++ includes/footer.php
@@ -0,0 +1,87 @@
+<style>
+    .footer-box {
+        background-color: #7B4F2E;
+        bottom: 0;
+        height: auto;
+    }
+
+    footer span {
+        font-size: 1.8rem;
+        font-family: 'Caprasimo';
+        color: #C4956A;
+    }
+
+    .icon-box {
+        width: 100%;
+    }
+
+    .icon-box i {
+        font-size: 2rem;
+        color: #C4956A;
+    }
+
+    .pawster-box {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .pawster-box .logo-row {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 0.5rem;
+    }
+
+    .pawster-box,
+    .links-box,
+    .info-box {
+        border-radius: 1.2rem;
+        min-height: 100px;
+        width: 270px;
+        padding: 0.5rem;
+    }
+
+    .links-box p,
+    .info-box p {
+        font-family: 'Cause', cursive;
+        color: #F5E6D3;
+        font-size: 1.2rem;
+    }
+
+    .copyright {
+        font-family: 'Cause', cursive;
+        color: #F5E6D3;
+        font-size: 1.2rem;
+    }
+</style>
+<div class="container-fluid footer-box p-4">
+    <div class="d-flex justify-content-evenly">
+        <div class="pawster-box text-center">
+            <div class="logo-row">
+                <img src="resources/images/Logo.png" alt="Logo" style="height: 4rem;">
+                <span class="fw-bold">PAWSTER</span>
+            </div>
+            <div class="icon-box d-flex justify-content-evenly">
+                <i class="bi bi-facebook"></i>
+                <i class="bi bi-instagram"></i>
+                <i class="bi bi-linkedin"></i>
+                <i class="bi bi-tiktok"></i>
+            </div>
+        </div>
+        <div class="links-box text-start mt-3">
+            <p>Home</p>
+            <p>Adopt</p>
+            <p>Shop</p>
+            <p>About us</p>
+
+        </div>
+        <div class="info-box text-allign-start mt-3">
+            <p><i class="bi bi-geo-alt-fill"></i> Napocor Village, Tandang Sora, Quezon City</p>
+            <p><i class="bi bi-telephone-fill"></i> +63 374288382</p>
+            <p><i class="bi bi-envelope-fill"></i> pawster.adopt@gmail.com</p>
+        </div>
+    </div>
+
+    <p class="copyright text-center">©2026 Pawster. All rights reserved.</p>
+</div>
--- includes/headlinks.php
+++ includes/headlinks.php
@@ -6,6 +6,8 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Caprasimo&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Caprasimo&family=Convergence&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Caprasimo&family=Cause:wght@100..900&family=Convergence&display=swap" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
