3df8f14e4dad4ddb6bc2d6223f47373fa954acf7
Seeeiiiii <seitachi123@gmail.com>
2026-05-29 12:27:43 +0800

    frontend: Added login page 60% done

---
+++ login.php
@@ -0,0 +1,53 @@
+<DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . '/PAWSTER/includes/headlinks.php'); ?>
+        <link rel="stylesheet" href="resources/css/login.css">
+    </head>
+
+    <body>
+        <div class="first-container min-vh-100 d-flex align-items-center justify-content-center">
+            <div class="row custom-card overflow-hidden shadow">
+
+                <div class="col-md-6 left-panel p-3 d-flex flex-column justify-content-between">
+                    <div class="logo-area d-flex justify-content-center mb-4">
+                        <div class="logo d-flex align-items-center">
+                            <img src="resources/images/logo white.png" alt="Logo" style="height: 6rem;">
+                            <h2>PAWSTER</h2>
+                        </div>
+                    </div>
+
+                    <div class="second-container p-3 d-flex flex-column align-items-center">
+                        <div class="row align-items-center">
+                            <div class="col-5">
+                                <img src="resources/images/login dogcat.png" alt="Email Icon" style="height: 12rem;">
+                            </div>
+                            <div class="col-7 m-0 ps-5">
+                                <p class="medium m-0">Find you perfect furry companion and give them a loving forever home</p>
+                                <div class="paw-icon d-flex justify-content-end pt-5">
+                                    <img src="resources/images/paw icon.png" alt="Paw Icon" style="height: 2rem;">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <h3 class="text-start py-5 px-2">Adopt, shop & care - All in one place. </h3>
+                </div>
+
+                <div class="col-md-6 right-panel p-5">
+                    <div class="d-flex mb-3 toggle-container">
+                        <button class="btn btn-toggle active flex-grow-1">Login</button>
+                        <button class="btn btn-toggle flex-grow-1">Sign up</button>
+                    </div>
+
+                    <h2 class="medium text-start">Welcome back!</h2>
+                    <h3 class="small text-start mb-4">Login to your Pawster account</h3>
+                </div>
+
+                <form>
+                    
+                </form>
+
+            </div>
+    </body>
