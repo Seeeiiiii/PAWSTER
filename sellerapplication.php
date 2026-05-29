8ae711b79c2f9ec12c29b3f5387d26a40b5d5160
Denzel027 <denzeltdomino@iskolarngbayan.pup.edu.ph>
2026-05-29 18:15:36 +0800

    Finished seller application page

--- sellerapplication.php
+++ sellerapplication.php
@@ -64,8 +64,52 @@
         </span>
 
         <hr class="section-divider mt-5">
-        
+
+        <h4>Upload Documents :</h4>
+        <div class="d-flex gap-5 mt-3 text-center">
+            <div class="p-3 business-permit flex-fill rounded-5">
+                <i class="bi bi-file-earmark-plus"></i>
+                <div class="h5 mt-2">Upload photo of business permit</div>
+                <div>Pdf or image , maximum of 5mb</div>
+            </div>
+            <div class="p-3 gov-id flex-fill rounded-5">
+                <i class="bi bi-person-vcard-fill"></i>
+                <div class="h5 mt-2">Upload photo of government ID</div>
+                <div>Pdf or image , maximum of 5mb</div>
+            </div>
+        </div>
+
+        <div class="d-flex flex-column align-items-center justify-content-center p-4">
+            <div class="form-check">
+                <input class="form-check-input me-2" type="checkbox" id="confirm1">
+                <label class="form-check-label" for="confirm1">
+                    I confirm that all information provided is accurate and complete, and I agree to Pawster's seller
+                    terms and conditions.
+                </label>
+            </div>
+
+            <div class="form-check">
+                <input class="form-check-input me-2" type="checkbox" id="confirm2">
+                <label class="form-check-label" for="confirm2">
+                    I understand that my application may be rejected if documents are incomplete or invalid.
+                </label>
+            </div>
+        </div>
+
+        <div class="d-flex align-items-between mt-3">
+            <div class="col"><button class="btn btn-cancel">
+                <i class="bi bi-arrow-left"></i>
+                Cancel Application
+            </button></div>
+            <div class="col text-end"><button class="btn btn-submit">
+                Submit Application
+                <i class="bi bi-arrow-right"></i>
+            </button></div>
+        </div>
     </div>
+
+
+
 
     <footer>
         <?php include($_SERVER['DOCUMENT_ROOT'] . '/PAWSTER/includes/footer.php'); ?>
