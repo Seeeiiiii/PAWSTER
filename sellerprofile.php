dfce51dd3feaef3134dda24734aa40115823353f
Denzel027 <denzeltdomino@iskolarngbayan.pup.edu.ph>
2026-06-04 01:48:45 +0800

    Little fix on the sellerprofile dropdown not working

--- sellerprofile.php
+++ sellerprofile.php
@@ -156,5 +156,9 @@ function renderStars(int $count): string {
 
     </main>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+    <footer>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . '/PAWSTER/includes/footer.php'); ?>
+    </footer>
 </body>
 </html>
