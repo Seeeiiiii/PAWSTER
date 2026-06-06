eda77f19715a4ba19f8929794dafa5a68f4c6f39
Seeeiiiii <seitachi123@gmail.com>
2026-06-06 15:09:05 +0800

    Fixed navbar issues and added test product for shop

--- role.php
+++ role.php
@@ -1,6 +1,8 @@
+<?php
+include($_SERVER['DOCUMENT_ROOT'] . '/PAWSTER/config/app.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
-
 
 <head>
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/PAWSTER/includes/headlinks.php'); ?>
@@ -70,3 +72,5 @@
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
+</html>
